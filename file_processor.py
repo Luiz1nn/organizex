6afb3f35a0a7c0f204b1ca3a36a5cf6ba805a780
Luis Fernando <luisfernandobr2003@gmail.com>
2024-11-09 22:50:37 -0300

    feat: convert to xls

--- file_processor.py
+++ file_processor.py
@@ -1,20 +1,26 @@
 import pandas as pd
 from pandas import DataFrame
+from utils.convert_to_xls import convert_to_xls
 
 def process_file(input_file: str, output_file: str) -> None:
-    df: DataFrame = pd.read_excel(input_file)
+    try:
+        df: DataFrame = pd.read_excel(input_file)
 
-    filter_keywords: list[str] = ['Saldo Anterior', 'Saldo do dia', 'S A L D O']
-    df_filtered: DataFrame = df[~df.isin(filter_keywords).any(axis=1)]
+        filter_keywords: list[str] = ['Saldo Anterior', 'Saldo do dia', 'S A L D O']
+        df_filtered: DataFrame = df[~df.isin(filter_keywords).any(axis=1)]
 
-    df_final: DataFrame = pd.DataFrame({
-        'Data': df_filtered['Data'],
-        'Descrição': df_filtered['Detalhes'],
-        'Categoria': '',
-        'Valor': df_filtered['Valor'],
-        'Situação': ''
-    })
+        df_final: DataFrame = pd.DataFrame({
+            'Data': df_filtered['Data'],
+            'Descrição': df_filtered['Detalhes'],
+            'Categoria': '',
+            'Valor': df_filtered['Valor'],
+            'Situação': ''
+        })
 
-    df_final.to_excel(output_file, index=False)
+        convert_to_xls(df_final, output_file)
 
-    print(f"Arquivo salvo em: {output_file}")
+        print(f"Arquivo salvo em: {output_file}")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
---
+++ utils/convert_to_xls.py
@@ -0,0 +1,12 @@
+import xlwt
+from pandas import DataFrame
+
+def convert_to_xls(df: DataFrame, output_file: str) -> None:
+    workbook_xls = xlwt.Workbook()
+    sheet_xls = workbook_xls.add_sheet("OrganizeX")
+
+    for row_idx, row in enumerate(df.to_records(index=False)):
+        for col_idx, cell_value in enumerate(row):
+            sheet_xls.write(row_idx, col_idx, str(cell_value))
+
+    workbook_xls.save(output_file)
