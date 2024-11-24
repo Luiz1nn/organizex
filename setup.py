2373df35771af5964faef7f26197011ef218f3f0
Luis Fernando <luisfernandobr2003@gmail.com>
2024-11-24 00:48:43 -0300

    refactor: pipenv manager

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from pipenv_manager.check_and_install import check_and_install_pipenv
-from pipenv_manager.run_commands import run_pipenv_commands
+from pipenv_manager import check_and_install_pipenv, run_pipenv_commands
 
 
 def setup():
