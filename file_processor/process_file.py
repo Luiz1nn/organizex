902398b7694d06c3860111adfb0c1153b83cb343
Luis Fernando <luisfernandobr2003@gmail.com>
2024-11-24 00:41:21 -0300

    refactor: file processor

---
+++ file_processor/process_file.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from pandas import DataFrame
+from check_and_fill_empty_details import check_and_fill_empty_details
+from convert_to_xls import convert_to_xls
+from update_details_for_credit_payment import update_details_for_credit_payment
+from utils import (
+    normalize_text,
+    remove_dates,
+    remove_sequential_numbers,
+    remove_times,
+    remove_extra_spaces,
+)
+
+
+def process_file(input_file: str, output_file: str) -> None:
+    try:
+        df: DataFrame = pd.read_excel(input_file)
+
+        check_and_fill_empty_details(df, 'Detalhes')
+
+        update_details_for_credit_payment(df)
+
+        filter_keywords: list[str] = [
+            'Saldo Anterior', 'Saldo do dia', 'S A L D O'
+        ]
+
+        df_filtered: DataFrame = df[~df.isin(filter_keywords).any(axis=1)]
+
+        df_filtered.loc[:, 'Detalhes'] = (
+            df_filtered['Detalhes']
+            .apply(remove_dates)
+            .apply(remove_times)
+            .apply(remove_sequential_numbers)
+            .apply(remove_extra_spaces)
+            .apply(normalize_text)
+        )
+
+        df_final: DataFrame = pd.DataFrame({
+            'Data': df_filtered['Data'],
+            'Descrição': df_filtered['Detalhes'],
+            'Categoria': '',
+            'Valor': df_filtered['Valor'],
+            'Situação': ''
+        })
+
+        convert_to_xls(df_final, output_file)
+
+        print(f"Arquivo salvo em: {output_file}")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,23 +1,17 @@
-from .check_and_fill_empty_details import check_and_fill_empty_details
-from .convert_to_xls import convert_to_xls
-from .ensure_normalizations import ensure_normalizations_json
+from .ensure_normalizations import ensure_normalizations
 from .normalize_text import normalize_text
 from .remove_dates import remove_dates
 from .remove_extra_spaces import remove_extra_spaces
 from .remove_sequential_numbers import remove_sequential_numbers
 from .remove_times import remove_times
 from .setup_warnings import setup_warnings
-from .update_details_for_credit_payment import update_details_for_credit_payment
 
 __all__ = [
-    'check_and_fill_empty_details',
-    'convert_to_xls',
-    'ensure_normalizations_json',
+    'ensure_normalizations',
     'normalize_text',
     'remove_dates',
     'remove_extra_spaces',
     'remove_sequential_numbers',
     'remove_times',
     'setup_warnings',
-    'update_details_for_credit_payment'
 ]
