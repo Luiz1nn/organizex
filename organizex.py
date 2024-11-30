e20524a61e2583a63e67a3e61efd1e3f295cdcf2
Luis Fernando <luisfernandobr2003@gmail.com>
2024-11-30 14:57:06 -0300

    feat: animate text

--- organizex.py
+++ organizex.py
@@ -1,12 +1,14 @@
 from cli import create_parser
 from file_processor import process_file
 from utils import ensure_categories, ensure_normalizations, setup_warnings
+from animate_text import animate_text
 
 
 __version__ = "1.7.0"
 
 
 def main():
+    animate_text()
     print(f"Versão do projeto: {__version__}")
     setup_warnings()
     ensure_normalizations()
